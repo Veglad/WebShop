53342da16056ddb766715e6fff18d09e9e877cc2
Vladyslav <rek0ra921@gmail.com>
2019-02-25 12:57:02 +0200

    goods_list > Add product list adapter, Picasso dependency

--- app/src/main/java/com/example/vshcheglov/webshop/domain/Product.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/domain/Product.kt
@@ -1,7 +1,7 @@
 package com.example.vshcheglov.webshop.domain
 
 data class Product(val id: Int,
-              val name: String,
-              val shortDescription: String,
-              val price: Double,
-              val imageUrl: String)
+                   val name: String,
+                   val description: String,
+                   val price: Double,
+                   val imageUrl: String)
---
+++ app/src/main/java/com/example/vshcheglov/webshop/ui/adapters/ProductsRecyclerAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.vshcheglov.webshop.ui.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.vshcheglov.webshop.R
+import com.example.vshcheglov.webshop.domain.Product
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.product_recycler_item.view.*
+
+class ProductsRecyclerAdapter(private val productList: List<Product>)
+    : RecyclerView.Adapter<ProductsRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.product_recycler_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = productList.size
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindProduct(productList[position])
+    }
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bindProduct(product: Product) {
+            with(product) {
+                Picasso.get().load(imageUrl).into(view.productImage)
+                view.productTitle.text = name
+                view.productDescription.text = description
+                view.productPrice.text = String.format(view.context.getString(R.string.price_format), price)
+            }
+        }
+    }
+}
