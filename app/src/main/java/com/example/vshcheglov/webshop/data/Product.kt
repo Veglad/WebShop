49addf120a959373535efb027843d4540aec74ba
Vladyslav <rek0ra921@gmail.com>
2019-02-25 12:33:53 +0200

    goods_list > Refactor Goods to Product, Add Product data class

---
+++ app/src/main/java/com/example/vshcheglov/webshop/data/Product.kt
@@ -0,0 +1,7 @@
+package com.example.vshcheglov.webshop.data
+
+data class Product(val id: Int,
+              val name: String,
+              val shortDescription: String,
+              val price: Double,
+              val imageUrl: String)
