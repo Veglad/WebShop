9fede9e8a67ee1d88cb1c29bd679a6b952eba1e8
Vladyslav <rek0ra921@gmail.com>
2019-02-25 13:45:06 +0200

    goods_list > Init recyclerView and fill with fakeProducts functoin

--- app/src/main/java/com/example/vshcheglov/webshop/ui/MainActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/ui/MainActivity.kt
@@ -2,12 +2,31 @@ package com.example.vshcheglov.webshop.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import com.example.vshcheglov.webshop.R
+import com.example.vshcheglov.webshop.domain.Product
+import com.example.vshcheglov.webshop.ui.adapters.ProductsRecyclerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val productsRecyclerAdapter = ProductsRecyclerAdapter(fakeProducts())
+        with(productsRecyclerView){
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = productsRecyclerAdapter
+        }
+    }
+
+    private fun fakeProducts() = mutableListOf<Product>().apply {
+        add(Product(0, "Iphone X", "Some description about IPhone", 1099.0, "https://i.allo.ua/media/catalog/product/cache/1/image/425x295/799896e5c6c37e11608b9f8e1d047d15/a/p/apple_iphone_x_64gb_mqac2_space_grey_4.jpg"))
+        add(Product(1, "Iphone XS", "Some description about IPhone XS", 1200.0, "https://hotline.ua/img/tx/178/178801049_s265.jpg"))
+        add(Product(0, "Iphone X", "Some description about IPhone", 1099.0, "https://i.allo.ua/media/catalog/product/cache/1/image/425x295/799896e5c6c37e11608b9f8e1d047d15/a/p/apple_iphone_x_64gb_mqac2_space_grey_4.jpg"))
+        add(Product(1, "Iphone XS", "Some description about IPhone XS", 1200.0, "https://hotline.ua/img/tx/178/178801049_s265.jpg"))
+        add(Product(0, "Iphone X", "Some description about IPhone", 1099.0, "https://i.allo.ua/media/catalog/product/cache/1/image/425x295/799896e5c6c37e11608b9f8e1d047d15/a/p/apple_iphone_x_64gb_mqac2_space_grey_4.jpg"))
+        add(Product(1, "Iphone XS", "Some description about IPhone XS", 1200.0, "https://hotline.ua/img/tx/178/178801049_s265.jpg"))
     }
 }
