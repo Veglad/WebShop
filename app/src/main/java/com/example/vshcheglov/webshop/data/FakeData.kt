e08060687406f3931946d2bf209cdf51a3a61849
Vladyslav <rek0ra921@gmail.com>
2019-02-25 16:54:28 +0200

    goods_list > Apply code style changes

--- app/src/main/java/com/example/vshcheglov/webshop/data/FakeData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.vshcheglov.webshop.data
-
-import android.content.Context
-import com.beust.klaxon.Klaxon
-import com.example.vshcheglov.webshop.domain.Product
-
-fun getFakeProducts(context: Context) =
-    Klaxon().parseArray<Product>(context.assets.open("products.json"))
--- app/src/main/java/com/example/vshcheglov/webshop/ui/MainActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/ui/MainActivity.kt
@@ -4,8 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
+import com.beust.klaxon.Klaxon
 import com.example.vshcheglov.webshop.R
-import com.example.vshcheglov.webshop.data.getFakeProducts
+import com.example.vshcheglov.webshop.domain.Product
 import com.example.vshcheglov.webshop.ui.adapters.ProductsRecyclerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val products = getFakeProducts(applicationContext)
+        val products = Klaxon().parseArray<Product>(applicationContext.assets.open("products.json"))
         if (products == null) {
             Toast.makeText(this, "Reading data error", Toast.LENGTH_SHORT).show()
         } else {
