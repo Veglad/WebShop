ee858354e2ac5d6dce3c73433b87d6f76ff369ac
Vladyslav <rek0ra921@gmail.com>
2019-02-28 20:43:36 +0200

    basket > Add back button to BasketActivity

--- app/src/main/java/com/example/vshcheglov/webshop/BasketActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/BasketActivity.kt
@@ -3,6 +3,7 @@ package com.example.vshcheglov.webshop
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.MenuItem
 import com.example.vshcheglov.webshop.domain.Basket
 import com.example.vshcheglov.webshop.ui.adapters.BasketRecyclerAdapter
 import kotlinx.android.synthetic.main.activity_basket.*
@@ -15,6 +16,7 @@ class BasketActivity : AppCompatActivity() {
 
         initRecyclerView()
         initOrderTextViews()
+        initActionBar()
     }
 
     private fun initRecyclerView() {
@@ -27,5 +29,19 @@ class BasketActivity : AppCompatActivity() {
     private fun initOrderTextViews() {
         basketAmountTextView.text = String.format(getString(R.string.price_format), Basket.totalPrice)
         basketItemsTextView.text = Basket.size.toString()
+    }
+
+    private fun initActionBar() {
+        supportActionBar?.also {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return true
     }
 }
