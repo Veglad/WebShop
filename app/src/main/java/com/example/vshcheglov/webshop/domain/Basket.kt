7ffd07c0c96e229304c2b0616dd358e68d0c2107
Vladyslav <rek0ra921@gmail.com>
2019-02-28 17:37:27 +0200

    basket > Update basket model

--- app/src/main/java/com/example/vshcheglov/webshop/domain/Basket.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/domain/Basket.kt
@@ -1,7 +1,8 @@
 package com.example.vshcheglov.webshop.domain
 
 object Basket {
-    private var productList = mutableListOf<Product>()
+    var productList = mutableListOf<Product>()
+        private set
     
     var size = 0
         get() = productList.size
@@ -16,31 +17,4 @@ object Basket {
             }
         }
         private set
-
-    fun clear() {
-        productList.clear()
-        totalPrice = 0.0
-    }
-
-    fun addProduct(product: Product) {
-        productList.add(product)
-    }
-
-    fun getProduct(index: Int) = if (isValidIndex(index)) {
-        productList[index]
-    } else {
-        null
-    }
-
-    fun removeProduct(product: Product) = productList.remove(product)
-
-    fun removeProductByPIndex(index: Int): Boolean {
-        if (isValidIndex(index)) {
-            productList.removeAt(index)
-            return true
-        }
-        return false
-    }
-
-    private fun isValidIndex(index: Int) = index >= 0 && index < productList.size
 }
