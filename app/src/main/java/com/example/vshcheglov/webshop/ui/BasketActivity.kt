4920c5e2008bd492d5df437cf13afd0f4e0cb5a0
Vladyslav <rek0ra921@gmail.com>
2019-03-01 19:17:53 +0200

    basket > Finish basket activity

--- app/src/main/java/com/example/vshcheglov/webshop/ui/BasketActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/ui/BasketActivity.kt
@@ -10,20 +10,24 @@ import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.MenuItem
 import com.example.vshcheglov.webshop.R
 import com.example.vshcheglov.webshop.domain.Basket
+import com.example.vshcheglov.webshop.domain.Product
 import com.example.vshcheglov.webshop.ui.adapters.BasketRecyclerAdapter
 import com.example.vshcheglov.webshop.ui.helpers.BasketRecyclerItemTouchHelper
 import kotlinx.android.synthetic.main.activity_basket.*
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class BasketActivity : AppCompatActivity(), BasketRecyclerItemTouchHelper.BasketRecyclerItemTouchHelperListener {
 
     lateinit var basketAdapter: BasketRecyclerAdapter
+    private var totalPriceWithDiscount = Basket.totalPriceWithDiscount
+    private var productListSize = Basket.productListSize
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basket)
 
         initRecyclerView()
-        initOrderTextViews(Basket.totalPriceWithDiscount, Basket.productListSize.toString())
+        initOrderTextViews(totalPriceWithDiscount, productListSize.toString())
         initActionBar()
     }
 
@@ -32,7 +36,9 @@ class BasketActivity : AppCompatActivity(), BasketRecyclerItemTouchHelper.Basket
             layoutManager = LinearLayoutManager(this@BasketActivity)
             basketAdapter = BasketRecyclerAdapter(this@BasketActivity).also {
                 it.onProductsNumberChangeListener = {
-                    initOrderTextViews(Basket.totalPriceWithDiscount, Basket.productListSize.toString())
+                    totalPriceWithDiscount = Basket.totalPriceWithDiscount
+                    productListSize = Basket.productListSize
+                    initOrderTextViews(totalPriceWithDiscount, productListSize.toString())
                 }
             }
             adapter = basketAdapter
@@ -62,15 +68,29 @@ class BasketActivity : AppCompatActivity(), BasketRecyclerItemTouchHelper.Basket
             val deletedIndex = viewHolder.adapterPosition
 
             basketAdapter.removeItem(position)
-
+            updateTotalSizeAndTotalPrice(mapPairToRemove, true)
+            basketMakeOrderButton.isEnabled = productListSize > 0
 
             val snackbar = Snackbar.make(basketMainConstraint, String.format(getString(R.string.removed_item_snackbar_format), removedItemName),
                 Snackbar.LENGTH_SHORT)
             snackbar.setAction(getString(R.string.undo_uppercase)){
                 basketAdapter.restoreItem(mapPairToRemove, deletedIndex)
+                updateTotalSizeAndTotalPrice(mapPairToRemove, false)
+                basketMakeOrderButton.isEnabled = productListSize > 0
             }
             snackbar.show()
         }
+    }
+
+    private fun updateTotalSizeAndTotalPrice(mapPairToRemove: Pair<Int, MutableList<Product>>, isRemoved: Boolean) {
+        if(isRemoved) {
+            totalPriceWithDiscount -= mapPairToRemove.second[0].priceWithDiscount * mapPairToRemove.second.size
+            productListSize -= mapPairToRemove.second.size
+        } else {
+            totalPriceWithDiscount += mapPairToRemove.second[0].priceWithDiscount * mapPairToRemove.second.size
+            productListSize += mapPairToRemove.second.size
+        }
+        initOrderTextViews(totalPriceWithDiscount, productListSize.toString())
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
