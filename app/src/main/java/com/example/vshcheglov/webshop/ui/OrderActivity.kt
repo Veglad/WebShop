a8403d3e6b93498e20c0155f78606eaca427a5bc
Vladyslav <rek0ra921@gmail.com>
2019-03-04 12:05:57 +0200

    basket > Apply changes to actionbar in OrderActivity

--- app/src/main/java/com/example/vshcheglov/webshop/ui/OrderActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/ui/OrderActivity.kt
@@ -1,8 +1,11 @@
 package com.example.vshcheglov.webshop.ui
 
+import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
+import android.view.MenuItem
 import com.example.vshcheglov.webshop.R
 import com.example.vshcheglov.webshop.domain.Basket
 import kotlinx.android.synthetic.main.activity_order.*
@@ -18,5 +21,26 @@ class OrderActivity : AppCompatActivity() {
             Snackbar.make(orderConstraintLayout, getString(R.string.order_completed),
                 Snackbar.LENGTH_SHORT).show()
         }
+
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(orderActionBar)
+        supportActionBar?.let {
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowTitleEnabled(false)
+        }
+        orderActionBar.navigationIcon?.setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_ATOP);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+        }
+
+        return false
     }
 }
