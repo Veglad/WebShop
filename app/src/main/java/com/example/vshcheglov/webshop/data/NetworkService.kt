94180180522ecfdbe09777a3f27686d64a41affd
Vladyslav <rek0ra921@gmail.com>
2019-03-06 12:26:41 +0200

    architecture > Rename Product data source interface

--- app/src/main/java/com/example/vshcheglov/webshop/data/NetworkService.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/data/NetworkService.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-object NetworkService : ShopProductService {
+object NetworkService : IProductDataSource {
     private const val BASE_URL = "https://us-central1-webshop-58013.cloudfunctions.net"
 
     private val webShopApi: WebShopApi by lazy {
