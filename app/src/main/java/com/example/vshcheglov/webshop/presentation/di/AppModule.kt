05e5ad0ebcc3189e26be58a86a0332a6c25bb091
Vladyslav <rek0ra921@gmail.com>
2019-03-13 19:14:22 +0200

    architecture  > Add AppModule

---
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.example.vshcheglov.webshop.presentation.di
+
+import android.app.Application
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class AppModule(private val application: Application) {
+
+    @Provides
+    @Singleton
+    fun providesApplication() = application
+}
