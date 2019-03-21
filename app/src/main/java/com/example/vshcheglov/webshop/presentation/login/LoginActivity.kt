7dcca6b8bf77ac8421aeeb6644196f7d740e06da
Vladyslav <rek0ra921@gmail.com>
2019-03-21 18:05:17 +0200

    login > Redevelop login presentation with MVP

--- app/src/main/java/com/example/vshcheglov/webshop/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/login/LoginActivity.kt
@@ -1,13 +1,17 @@
 package com.example.vshcheglov.webshop.presentation.login
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vshcheglov.webshop.R
+import nucleus5.factory.RequiresPresenter
+import nucleus5.view.NucleusAppCompatActivity
 
-class LoginActivity : AppCompatActivity() {
+@RequiresPresenter(LoginPresenter::class)
+class LoginActivity : NucleusAppCompatActivity<LoginPresenter>(), LoginPresenter.PresenterView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        presenter.checkIfUserAuthorized()
     }
 }
---
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/login/LoginPresenter.kt
@@ -0,0 +1,17 @@
+package com.example.vshcheglov.webshop.presentation.login
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import nucleus5.presenter.Presenter
+
+class LoginPresenter: Presenter<LoginPresenter.PresenterView>() {
+
+    fun checkIfUserAuthorized() {
+
+    }
+
+
+    interface PresenterView {
+
+    }
+}
