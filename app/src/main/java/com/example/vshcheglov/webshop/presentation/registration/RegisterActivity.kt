c9e4fdc3611c63735675e8c8e27d703df96ac544
Vladyslav <rek0ra921@gmail.com>
2019-03-22 15:55:35 +0200

    login > register activity init

---
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/registration/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vshcheglov.webshop.presentation.registration
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
