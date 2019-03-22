b1d18487459eb0fd0bd5a7e8fa66da427627b3bf
Vladyslav <rek0ra921@gmail.com>
2019-03-22 15:59:43 +0200

    login > Create register layout

--- app/src/main/java/com/example/vshcheglov/webshop/presentation/registration/RegisterActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/registration/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.vshcheglov.webshop.presentation.registration
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vshcheglov.webshop.R
 
 class RegisterActivity : AppCompatActivity() {
 
