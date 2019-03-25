d206ad9bb5d2215e2113eb65f644e17c4049b62c
Vladyslav <rek0ra921@gmail.com>
2019-03-25 11:06:06 +0200

    login > Add multidex

--- app/src/main/java/com/example/vshcheglov/webshop/presentation/di/modules/NetworkModule.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/di/modules/NetworkModule.kt
@@ -1,21 +1,11 @@
 package com.example.vshcheglov.webshop.presentation.di.modules
 
 import android.content.Context
-import com.example.vshcheglov.webshop.data.enteties.mappers.ProductEntityDataMapper
 import com.example.vshcheglov.webshop.data.network.NetworkService
 import com.example.vshcheglov.webshop.data.network.WebShopApi
 import com.example.vshcheglov.webshop.data.products.NetworkDataSource
-import com.example.vshcheglov.webshop.extensions.isNetworkAvailable
 import dagger.Module
 import dagger.Provides
-import okhttp3.Cache
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.io.File
 import javax.inject.Singleton
 
 @Module(includes = [AppModule::class, MappersModule::class])
