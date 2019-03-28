2b7f3726f8804bef1e08c53711b06f348619d11a
Vladyslav <rek0ra921@gmail.com>
2019-03-28 18:50:10 +0200

    order_screen > Update order layout

--- app/src/main/java/com/example/vshcheglov/webshop/presentation/order/OrderActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/order/OrderActivity.kt
@@ -6,7 +6,9 @@ import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
 import android.view.MenuItem
 import com.example.vshcheglov.webshop.R
+import com.example.vshcheglov.webshop.extensions.isNetworkAvailable
 import kotlinx.android.synthetic.main.activity_order.*
+import kotlinx.android.synthetic.main.activity_register.*
 import nucleus5.factory.RequiresPresenter
 import nucleus5.view.NucleusAppCompatActivity
 
@@ -18,10 +20,28 @@ class OrderActivity : NucleusAppCompatActivity<OrderPresenter>(), OrderPresenter
         setContentView(R.layout.activity_order)
 
         buttonOrder.setOnClickListener {
-            Snackbar.make(orderLinearLayout, getString(R.string.order_completed), Snackbar.LENGTH_SHORT).show()
+            val name = orderName.text.toString()
+            val lastName = orderLastName.text.toString()
+            val cardNumber = orderCardNumber.text.toString()
+            val cardMonth = orderCardMonth.text.toString().toIntOrNull()
+            val cardYear = orderCardYear.text.toString().toIntOrNull()
+            val cardCv = orderCardCV.text.toString()
+
+            clearErrors()
+
+            presenter.makeOrder(name, lastName, cardNumber, cardMonth, cardYear, cardCv, isNetworkAvailable())
         }
 
         initActionBar()
+    }
+
+    private fun clearErrors() {
+        nameTextInput.error = ""
+        lastNameTextInput.error = ""
+        cardNumberTextInput.error = ""
+        cardMonthTextInput.error = ""
+        cardYearTextInput.error = ""
+        cardCvTextInput.error = ""
     }
 
     override fun onResume() {
@@ -52,5 +72,49 @@ class OrderActivity : NucleusAppCompatActivity<OrderPresenter>(), OrderPresenter
 
     override fun setOrderPrice(orderPrice: Double) {
         orderTotalPrice.text = String.format(getString(R.string.price_format), orderPrice)
+    }
+
+    override fun setShowProgress(isVisible: Boolean) {
+        if (isVisible) {
+            buttonOrder.startAnimation()
+        } else {
+            buttonOrder.revertAnimation()
+        }
+    }
+
+    override fun showInvalidName() {
+        nameTextInput.error = resources.getString(R.string.order_invalid_name)
+    }
+
+    override fun showInvalidSecondName() {
+        lastNameTextInput.error = resources.getString(R.string.order_invalid_last_name)
+    }
+
+    override fun showInvalidCardNumber() {
+        cardNumberTextInput.error = resources.getString(R.string.order_invalid_card_number)
+    }
+
+    override fun showInvalidCardMonth() {
+        cardMonthTextInput.error = resources.getString(R.string.order_invalid_card_month)
+    }
+
+    override fun showInvalidCardYear() {
+        cardYearTextInput.error = resources.getString(R.string.order_invalid_card_year)
+    }
+
+    override fun showInvalidCardCv() {
+        cardCvTextInput.error = resources.getString(R.string.order_invalid_cv)
+    }
+
+    override fun showNoInternetError() {
+        Snackbar.make(
+            orderLinearLayout,
+            resources.getString(R.string.no_internet_connection_warning),
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+    override fun notifyOrderCompleted() {
+        Snackbar.make(orderLinearLayout, getString(R.string.order_completed), Snackbar.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/vshcheglov/webshop/presentation/order/OrderPresenter.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/order/OrderPresenter.kt
@@ -1,9 +1,19 @@
 package com.example.vshcheglov.webshop.presentation.order
 
 import com.example.vshcheglov.webshop.domain.Basket
+import com.example.vshcheglov.webshop.extensions.isCardNumberValid
+import com.example.vshcheglov.webshop.extensions.isCvValid
 import nucleus5.presenter.Presenter
+import java.util.*
 
 class OrderPresenter : Presenter<OrderPresenter.OrderView>() {
+
+    companion object {
+        const val MIN_NAME_LENGTH = 2
+        const val MIN_CARD_MONTH_NUMBER = 1
+        const val MAX_CARD_MONTH_NUMBER = 12
+        const val MAX_CARD_YEAR_NUMBER = 30
+    }
 
     fun initOrderPrice() {
         val orderPrice = Basket.totalPriceWithDiscount
@@ -11,7 +21,75 @@ class OrderPresenter : Presenter<OrderPresenter.OrderView>() {
 
     }
 
+    fun makeOrder(
+        name: String, lastName: String, cardNumber: String,
+        cardMonth: Int?, cardYear: Int?, cardCv: String, isNetowrkAvaliable: Boolean
+    ) {
+        var isValid = true
+        view?.let {
+            it.setShowProgress(true)
+
+            if (name.length < MIN_NAME_LENGTH) {
+                isValid = false
+                it.showInvalidName()
+            }
+            if (lastName.length < MIN_NAME_LENGTH) {
+                isValid = false
+                it.showInvalidSecondName()
+            }
+            if (!cardNumber.isCardNumberValid()) {
+                isValid = false
+                it.showInvalidCardNumber()
+            }
+            if (cardMonth == null || cardMonth !in MIN_CARD_MONTH_NUMBER..MAX_CARD_MONTH_NUMBER) {
+                isValid = false
+                it.showInvalidCardMonth()
+            }
+            if (cardYear == null ||
+                cardYear !in Calendar.getInstance().get(Calendar.YEAR) % 100..MAX_CARD_YEAR_NUMBER) {
+                isValid = false
+                it.showInvalidCardYear()
+            }
+            if (!cardCv.isCvValid()) {
+                isValid = false
+                it.showInvalidCardCv()
+            }
+
+            if (isNetowrkAvaliable) {
+                if (isValid) {
+                    it.notifyOrderCompleted()
+                    it.setShowProgress(false)
+                } else {
+                    it.setShowProgress(false)
+                }
+            } else {
+                it.showNoInternetError()
+                it.setShowProgress(false)
+            }
+        }
+
+
+    }
+
     interface OrderView {
         fun setOrderPrice(orderPrice: Double)
+
+        fun setShowProgress(isVisible: Boolean)
+
+        fun showInvalidName()
+
+        fun showInvalidSecondName()
+
+        fun showInvalidCardNumber()
+
+        fun showInvalidCardMonth()
+
+        fun showInvalidCardYear()
+
+        fun showInvalidCardCv()
+
+        fun showNoInternetError()
+
+        fun notifyOrderCompleted()
     }
 }
