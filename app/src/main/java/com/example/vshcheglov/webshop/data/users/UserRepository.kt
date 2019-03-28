cde661ff962deeacd4ab1c6631781dd8475df5f1
Vladyslav <rek0ra921@gmail.com>
2019-03-28 12:17:41 +0200

    firebase_db > Replace Realtime database with firebase

--- app/src/main/java/com/example/vshcheglov/webshop/data/users/UserRepository.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/data/users/UserRepository.kt
@@ -5,7 +5,6 @@ import com.example.vshcheglov.webshop.data.enteties.User
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -19,9 +18,7 @@ class UserRepository : UserStorage {
     @Inject
     lateinit var firebaseAuth: FirebaseAuth
     @Inject
-    lateinit var firebaseDatabase: FirebaseDatabase
-    @Inject
-    lateinit var firebaseFirestore: FirebaseFirestore
+    lateinit var firestore: FirebaseFirestore
 
     override val isSignedIn: Boolean
         get() = firebaseAuth.currentUser != null
@@ -32,8 +29,7 @@ class UserRepository : UserStorage {
 
     override fun registerUserWithEmailAndPassword(
         email: String, password: String,
-        completeCallback: (task: Task<AuthResult>) -> Unit
-    ) {
+        completeCallback: (task: Task<AuthResult>) -> Unit) {
 
         firebaseAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -41,8 +37,7 @@ class UserRepository : UserStorage {
                 if (task.isSuccessful) {
                     val user = firebaseAuth.currentUser
                     if (user != null) {
-                        val userReference = firebaseDatabase.getReference("users").child(user.uid)
-                        userReference.setValue(User(user.email, user.uid))
+                        firestore.collection("users").document(user.uid).set(User(user.email, user.uid))
                         Timber.d("Added uid to Firestore")
                     } else {
                         Timber.d("User id saving error")
@@ -53,39 +48,25 @@ class UserRepository : UserStorage {
 
     override fun signInUserWithEmailAndPassword(
         email: String, password: String,
-        completeCallback: (task: Task<AuthResult>) -> Unit
-    ) {
+        completeCallback: (task: Task<AuthResult>) -> Unit) {
 
         firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                completeCallback(task)
-                if (task.isSuccessful) {
-                    val user = firebaseAuth.currentUser
-                    if (user != null) {
-                        val userReference = firebaseDatabase.getReference("users").child(user.uid)
-                        userReference.setValue(User(user.email, user.uid))
-                        Timber.d("Added uid to Firestore")
-                    } else {
-                        Timber.d("User id saving error")
-                    }
-                }
-            }
+            .addOnCompleteListener { task -> completeCallback(task) }
     }
 
     override fun getCurrentUser(processUser: (user: User?) -> Unit) {
         val currentUser = firebaseAuth.currentUser
-        var user: User?
         if (currentUser != null) {
-            val userReference = firebaseDatabase.getReference("users").child(currentUser.uid)
-            userReference.addListenerForSingleValueEvent(object: ValueEventListener{
-                override fun onCancelled(dataError: DatabaseError) {}
-
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    user = dataSnapshot.getValue<User>(User::class.java)
+            firestore.collection("users")
+                .document(currentUser.uid)
+                .get()
+                .addOnSuccessListener { document ->
+                    val user = document?.toObject(User::class.java)
                     processUser(user)
                 }
-
-            })
+                .addOnFailureListener {
+                    processUser(null)
+                }
         } else {
             processUser(null)
         }
--- app/src/main/java/com/example/vshcheglov/webshop/presentation/di/modules/FirebaseModule.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/di/modules/FirebaseModule.kt
@@ -1,8 +1,6 @@
 package com.example.vshcheglov.webshop.presentation.di.modules
 
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
@@ -14,10 +12,6 @@ class FirebaseModule {
     @Singleton
     @Provides
     fun provideFirebaseAuth() = FirebaseAuth.getInstance()
-
-    @Singleton
-    @Provides
-    fun provideFirebaseDatabase() = FirebaseDatabase.getInstance()
 
     @Singleton
     @Provides
