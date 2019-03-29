2b6d48ce4e24707e91ab94779b0eba69eb509163
Vladyslav <rek0ra921@gmail.com>
2019-03-29 17:13:32 +0200

    bought_products > Redevelop bought activity with usage of Nucleus

--- app/src/main/java/com/example/vshcheglov/webshop/presentation/bought/BoughtActivity.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/bought/BoughtActivity.kt
@@ -1,10 +1,12 @@
 package com.example.vshcheglov.webshop.presentation.bought
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vshcheglov.webshop.R
+import nucleus5.factory.RequiresPresenter
+import nucleus5.view.NucleusActivity
 
-class BoughtActivity : AppCompatActivity() {
+@RequiresPresenter(BoughtPresenter::class)
+class BoughtActivity : NucleusActivity<BoughtPresenter>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/bought/BoughtPresenter.kt
@@ -0,0 +1,12 @@
+package com.example.vshcheglov.webshop.presentation.bought
+
+import nucleus5.presenter.Presenter
+
+class BoughtPresenter : Presenter<BoughtPresenter.View>() {
+
+    
+
+    interface View {
+
+    }
+}
