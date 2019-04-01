e3dd6eb154303d1da3e63cfb1a80a750f8204f3a
Vladyslav <rek0ra921@gmail.com>
2019-04-01 18:14:28 +0300

    database > Inject RealmProductProduct mapper to ProductRepository

--- app/src/main/java/com/example/vshcheglov/webshop/data/products/ProductRepository.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/data/products/ProductRepository.kt
@@ -1,12 +1,15 @@
 package com.example.vshcheglov.webshop.data.products
 
 import com.example.vshcheglov.webshop.App
+import com.example.vshcheglov.webshop.data.enteties.mappers.RealmProductProductMapper
 import javax.inject.Inject
 
 class ProductRepository {
 
     @Inject
     lateinit var networkDataSource: ProductNetworkDataSource
+    @Inject
+    lateinit var RealmProductProductMapper: RealmProductProductMapper
 
     init {
         App.appComponent.inject(this)
--- app/src/main/java/com/example/vshcheglov/webshop/presentation/di/modules/MappersModule.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/presentation/di/modules/MappersModule.kt
@@ -2,6 +2,7 @@ package com.example.vshcheglov.webshop.presentation.di.modules
 
 import com.example.vshcheglov.webshop.data.enteties.mappers.BasketToOrderMapper
 import com.example.vshcheglov.webshop.data.enteties.mappers.ProductEntityDataMapper
+import com.example.vshcheglov.webshop.data.enteties.mappers.RealmProductProductMapper
 import com.example.vshcheglov.webshop.data.users.mappers.UserNetworkUserMapper
 import com.example.vshcheglov.webshop.presentation.entites.mappers.ProductBasketCardMapper
 import dagger.Module
@@ -26,4 +27,8 @@ class MappersModule {
     @Singleton
     @Provides
     fun provideUserNetworkUserMapper() = UserNetworkUserMapper()
+
+    @Singleton
+    @Provides
+    fun provideRealmProductProductMapper() = RealmProductProductMapper()
 }
