2e11e39ec9fd61333f93dd984d7272e7265379d7
Vladyslav <rek0ra921@gmail.com>
2019-04-02 19:44:35 +0300

    database > Create RealmOrderOrderMapper

---
+++ app/src/main/java/com/example/vshcheglov/webshop/data/enteties/mappers/RealmOrderOrderMapper.kt
@@ -0,0 +1,46 @@
+package com.example.vshcheglov.webshop.data.enteties.mappers
+
+import com.example.vshcheglov.webshop.data.enteties.OrderRealmProduct
+import com.example.vshcheglov.webshop.data.enteties.RealmOrder
+import com.example.vshcheglov.webshop.domain.Order
+import com.example.vshcheglov.webshop.domain.OrderProduct
+import com.example.vshcheglov.webshop.domain.common.Mapper
+import io.realm.RealmList
+
+class RealmOrderOrderMapper : Mapper<RealmOrder, Order> {
+    override fun map(from: RealmOrder): Order {
+        val productList = mutableListOf<OrderProduct>().apply {
+            for (realmProduct in from.orderProducts) {
+                add(map(realmProduct))
+            }
+        }
+
+        return Order(productList, from.timestamp, from.amount, from.id)
+    }
+
+    fun map(from: Order): RealmOrder {
+        val realmProductList = RealmList<OrderRealmProduct>().apply {
+            for (product in from.orderProducts) {
+                add(map(product))
+            }
+        }
+
+        return RealmOrder(realmProductList, from.timestamp, from.amount, from.id)
+    }
+
+    fun map(from: OrderRealmProduct) = OrderProduct(
+        from.id,
+        from.name,
+        from.price,
+        from.imageUrl,
+        from.count
+    )
+
+    fun map(from: OrderProduct) = OrderRealmProduct(
+        from.id,
+        from.name,
+        from.price,
+        from.imageUrl,
+        from.count
+    )
+}
