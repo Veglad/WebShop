c81439367d4aed0a260a4c19df06287323b953e8
Vladyslav <rek0ra921@gmail.com>
2019-04-02 18:40:54 +0300

    database > Update RealmOrder

--- app/src/main/java/com/example/vshcheglov/webshop/data/enteties/RealmOrder.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/data/enteties/RealmOrder.kt
@@ -1,10 +1,20 @@
 package com.example.vshcheglov.webshop.data.enteties
 
 import com.google.firebase.Timestamp
+import io.realm.RealmObject
 
 open class RealmOrder(
-    var orderProducts: MutableList<RealmProduct>,
+    var orderProducts: MutableList<OrderRealmProduct>,
     var timestamp: Timestamp,
     var amount: Double,
     var id: String = ""
-)
+) : RealmObject()
+
+
+open class OrderRealmProduct(
+    var id: Int = -1,
+    var name: String = "Noname",
+    var price: Double = 0.0,
+    var imageUrl: String = "",
+    var count: Int = 0) : RealmObject()
+
