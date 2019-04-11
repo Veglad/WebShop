45467105ee12a93875d1f8653c1691b8367f676d
Vladyslav <rek0ra921@gmail.com>
2019-04-11 15:48:37 +0300

    login_fingerprint > Update RealmUserCredentials class

--- app/src/main/java/com/example/vshcheglov/webshop/data/enteties/RealmUserCredentials.kt
+++ app/src/main/java/com/example/vshcheglov/webshop/data/enteties/RealmUserCredentials.kt
@@ -4,7 +4,8 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
 open class RealmUserCredentials(
-    @PrimaryKey
-    var id: Int = 1,
     var email: String = "",
-    var encryptedPassword: String = "") : RealmObject()
+    var encryptedPassword: String = "",
+    @PrimaryKey
+    var id: Int = 1
+) : RealmObject()
